e48fefc13368844ae57fdcd5a1acf3bec8ff315a
JossDer <jossuaderval3@gmail.com>
2026-05-21 16:14:24 +0200

    Update PierrePapierCiseaux.js

--- PierrePapierCiseaux.js
+++ PierrePapierCiseaux.js
@@ -2,21 +2,35 @@ const tableau = ["pierre", "papier", "ciseaux"];
 let choixUtilisateur = prompt(`Choisis : pierre, papier, ciseaux ou stop`)
   .trim()
   .toLowerCase();
-let choixPc = tableau[Math.floor(Math.random() * tableau.length)];
+let egalite = 0
+let victoire = 0
+let defaite = 0
 while (choixUtilisateur !== "stop") {
+  let choixPc = tableau[Math.floor(Math.random() * tableau.length)];
+    if (!tableau.includes(choixUtilisateur)) {
+    console.log("Choix invalide !");
+      choixUtilisateur = prompt("Choisis : pierre, papier, ciseaux ou stop")
+    .trim()
+    .toLowerCase();
+  } else {
+
   if (choixPc === choixUtilisateur) {
     console.log(`L'ordinateur a choisi ${choixPc}.\n Résultat : Egalité !`);
+    egalite++
   } else if (
     (choixPc === "pierre" && choixUtilisateur === "ciseaux") ||
     (choixPc === "ciseaux" && choixUtilisateur === "papier") ||
     (choixPc === "papier" && choixUtilisateur === "pierre")
   ) {
     console.log(`L'ordinateur a choisi ${choixPc}.\n Résultat : Perdu !`);
+    defaite++
   } else {
     console.log(`L'ordinateur a choisi ${choixPc}.\n Résultat : Gagné !`);
+    victoire++
   }
   choixUtilisateur = prompt(`Choisis : pierre, papier, ciseaux ou stop`)
     .trim()
     .toLowerCase();
-  choixPc = tableau[Math.floor(Math.random() * tableau.length)];
-}
+}} 
+console.log(`*****Résultats*****`)
+console.log(`Victoires : ${victoire}\n Défaites : ${defaite}\n Égalité : ${egalite}\n `)
